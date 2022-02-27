658fcc5531522046e4cec803a2c0a0ce8b7a269e
Sasho1256 <atmilachkov@gmail.com>
2022-02-27 11:48:18 +0200

    streamlined the database and added shopping cart model

---
+++ SubUrbanClothes/SubUrbanClothes.Database/CartItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubUrbanClothes.Database
+{
+    public class CartItem
+    {
+        [Key]
+        [Column("Item_Id")]
+        public string ItemId { get; set; }
+        
+        [Column("Cart_Id")]
+        public string CartId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public System.DateTime DateCreated { get; set; }
+        
+        [ForeignKey("Product")]
+        [Column("Product_Id")]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
