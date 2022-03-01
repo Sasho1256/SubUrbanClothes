d26e1a422919851adeb135c8ed23eec710cef79a
Valeri Tonchev <v.tonchev2003@gmail.com>
2022-03-01 19:26:28 +0200

    BrandService

---
+++ SubUrbanClothes/SubUrbanClothes.Web/Services/BrandService.cs
@@ -0,0 +1,29 @@
+﻿using SubUrbanClothes.Web.Models;
+using SubUrbanClothes.Web.Services.Contracts;
+
+namespace SubUrbanClothes.Web.Services
+{
+    public class BrandService : IBrandService
+    {
+
+        public void Create(Brand Brand)
+        {
+            if (string.IsNullOrEmpty(brand.BrandName) || string.IsNullOrWhiteSpace(brand.BrandName))
+            {
+                throw new ArgumentException("Incorrect input for brand name.");
+            }
+            List<Brand> brands = database.Brands.ToList();
+            if (!brands.Exists(d => d.BrandName == brand.BrandName))
+            {
+                database.Add(brand);
+            }
+            database.SaveChanges();
+        }
+
+        public List<Brand> GetAll()
+        {
+            List<Brand> brands = database.Directors.ToList();
+            return brands;
+        }
+    }
+}
---
+++ SubUrbanClothes/SubUrbanClothes.Web/Services/Contracts/IBrandService.cs
@@ -0,0 +1,19 @@
+﻿using SubUrbanClothes.Web.Models;
+
+namespace SubUrbanClothes.Web.Services.Contracts
+{
+    interface IBrandService
+    {
+        /// <summary>
+        /// Adds <paramref name="brand"/> to the database if a brand with the same name doesn't already exist. 
+        /// </summary>
+        /// <param name="brand"></param>
+        void Create(Brand brand);
+
+        /// <summary>
+        /// Returns a list with all the brands in the database.
+        /// </summary>
+        /// <returns></returns>
+        List<Brand> GetAll();
+    }
+}
