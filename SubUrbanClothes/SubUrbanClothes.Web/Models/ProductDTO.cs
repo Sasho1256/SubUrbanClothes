536fb252685c73663fa8bebc1ece47230a4ea83a
Sasho1256 <atmilachkov@gmail.com>
2022-03-02 18:25:37 +0200

    fixed productdto

---
+++ SubUrbanClothes/SubUrbanClothes.Web/Models/ProductDTO.cs
@@ -0,0 +1,29 @@
+﻿using SubUrbanClothes.Database;
+
+namespace SubUrbanClothes.Web.Models
+{
+    public class ProductDTO
+    {
+        public Product product { get; set; }
+        public List<Brand> Brands { get; set; }
+        public List<Category> Categories { get; set; }
+
+        public ProductDTO()
+        {
+
+        }
+
+        public ProductDTO(List<Brand> brands, List<Category> categories)
+        {
+            this.Brands = brands;
+            this.Categories = categories;
+        }
+
+        public ProductDTO(Product product, List<Brand> brands, List<Category> categories)
+        {
+            this.product = product;
+            this.Brands = brands;
+            this.Categories = categories;
+        }
+    }
+}
