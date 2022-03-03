6b77d26243e4e6826b02f2309a4d083ae56cd965
Valeri Tonchev <v.tonchev2003@gmail.com>
2022-03-04 00:23:00 +0200

    UserInterface(has errors)

--- SubUrbanClothes/SubUrbanClothes.Web/Controllers/UserController.cs
+++ SubUrbanClothes/SubUrbanClothes.Web/Controllers/UserController.cs
@@ -1,6 +1,118 @@
-﻿namespace SubUrbanClothes.Web.Controllers
+﻿using SubUrbanClothes.Services;
+using SubUrbanClothes.Database;
+using Microsoft.AspNetCore.Mvc;
+using SubUrbanClothes.Web.Models;
+
+namespace SubUrbanClothes.Web.Controllers
 {
-    public class UserController
+    public class UserController : Controller
     {
+        private UserService userService;
+
+        public UserController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public IActionResult UserProfile()
+        {
+            User user = new User();
+            try
+            {
+                user = this.userService.UserProfile();
+            }
+            catch (AccessViolationException ex)
+            {
+                return RedirectToAction("ExceptionHandling", "Exception", new ExMessDTO(ex.Message));
+            }
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user, ConfirmPasswordDTO confirmPassword1)
+        {
+            try
+            {
+                this.userService.Register(user, confirmPassword1);
+            }
+            catch (AccessViolationException ex)
+            {
+                return RedirectToAction("ExceptionHandling", "Exception", new ExMessDTO(ex.Message));
+            }
+            catch (ArgumentException ex)
+            {
+                return RedirectToAction("ExceptionHandling", "Exception", new ExMessDTO(ex.Message));
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LogIn(User user)
+        {
+            try
+            {
+                this.userService.LogIn(user);
+            }
+            catch (MissingMemberException ex)
+            {
+                return RedirectToAction("ExceptionHandling", "Exception", new ExMessDTO(ex.Message));
+            }
+            catch (AccessViolationException ex)
+            {
+                return RedirectToAction("ExceptionHandling", "Exception", new ExMessDTO(ex.Message));
+            }
+            catch (InvalidOperationException ex)
+            {
+                return RedirectToAction("ExceptionHandling", "Exception", new ExMessDTO(ex.Message));
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult LogOut()
+        {
+            userService.LogOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EditProfile(User user, ConfirmPasswordDTO confirmPassword1)
+        {
+            try
+            {
+                this.userService.EditProfile(user, confirmPassword1);
+            }
+            catch (ArgumentException ex)
+            {
+                return RedirectToAction("ExceptionHandling", "Exception", new ExMessDTO(ex.Message));
+            }
+            return RedirectToAction("UserProfile");
+        }
+
+        public IActionResult DeleteUser()
+        {
+            userService.DeleteUser();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult AreYouSure()
+        {
+            return View();
+        }
     }
 }
---
+++ SubUrbanClothes/SubUrbanClothes.Web/Models/ExMessDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace SubUrbanClothes.Web.Models
+{
+    public class ExMessDTO
+    {
+        public string ExMess { get; set; }
+
+        public ExMessDTO()
+        {
+
+        }
+
+        public ExMessDTO(string exMess)
+        {
+            ExMess = exMess;
+        }
+    }
+}
